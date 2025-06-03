9e3c2c1f1b30c35f18bc85cecbe0ba4a9f663843
Simonanchoa <siatehortuar@unal.edu.co>
2025-06-03 01:05:20 +0000

    ola

---
+++ W3School/primercodigo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int mynum= 15;
+
+int main() { // comentario re xd, hijueputas
+    cout << "Hello world\n";
+    cout << "Aprendiending c++" << "\n\n";
+    cout << 34.6 << endl;
+    cout << 3+6 << endl;
+    cout << "Mi nueva variable es:"<< mynum << endl;
+    return 0;
+}
