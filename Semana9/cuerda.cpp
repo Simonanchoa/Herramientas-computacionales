60c845ee7296666022c68023c33e8da4f48eabf1
Simonanchoa <siatehortuar@unal.edu.co>
2025-06-05 15:45:57 +0000

    entrega de hoy

--- Semana9/cuerda.cpp
+++ Semana9/cuerda.cpp
@@ -1,1 +1,24 @@
-#inlcude <iostream>
+#include <iostream>
+#include <cmath>
+#include <array>
+#include <string>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+double Upasado[101];
+
+double condinicial(double array){
+    for (int i=0; i<=101;i++){
+        if (i<51){
+            array[i]=0.02*i;
+        }else{
+            array[i]=-0.02*i+0.1;
+
+            }
+
+        
+
+    }
+}
