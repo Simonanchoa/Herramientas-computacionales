503c5112641dd908358187bb7cd69557d1c3fe76
Simonanchoa <siatehortuar@unal.edu.co>
2025-06-09 23:00:52 +0000

    avance

--- Semana9/graficas.py
+++ Semana9/graficas.py
@@ -1,5 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib as plt
 
-np.genfromtxt(posicioninicial.txt,skip_header=1) # type: ignore
+x=np.genfromtxt("posicioninicial.txt",skip_header=1) 
 
+pt=np.linspace(0,100,101)
+
+print("x:", x)
+print("Shape of x:", x.shape)
+
+
+plt.figure()
+plt.plot(pt,x)
+plt.show()
