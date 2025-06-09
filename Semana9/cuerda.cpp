a3db6e63b91393147ec1d3ad979310f678f5b315
Simonanchoa <siatehortuar@unal.edu.co>
2025-06-09 15:04:19 +0000

    cambio en casa

--- Semana9/cuerda.cpp
+++ Semana9/cuerda.cpp
@@ -7,23 +7,30 @@
 
 using namespace std;
 
-double* condinicial(double* array){
-    for (int i=0; i<=101;i++){
+double* condinicial(double* array, string nombre){
+    ofstream datos(nombre);
+    datos << fixed << setprecision(3);
+    datos << "posición"<< endl ;
+    for (int i=0; i<101;i++){
         if (i<51){
-            array[i]=0.02*i;
+            array[i]=0.002*i;
+
         }else{
-            array[i]=-0.02*i+0.1;
+            array[i]=(-0.002*i)+0.2;
+
 
             }
+        datos << array[i] << endl ;
 
         
 
     }
+    datos.close();
     return array;
 }
 
 int main(){
     double Upasado[101];
-    condinicial(Upasado);
+    condinicial(Upasado, "posicioninicial.txt");
     cout << Upasado;
 }
