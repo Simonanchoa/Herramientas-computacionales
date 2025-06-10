a34e1235818bb74335a76e05f4671978f0f0de0e
Simonanchoa <siatehortuar@unal.edu.co>
2025-06-10 15:57:23 +0000

    avance

--- Semana9/cuerda.cpp
+++ Semana9/cuerda.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <fstream>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
@@ -44,7 +45,7 @@ double*  uinit(double* array, double* aray, string archivo){
     unicial.close();
     return array;
 }
-double* paso(double* pasado,double* presente, double* futuro,string datos){
+double* paso(double* pasado,double* presente, double* futuro){
     futuro[0]=0;
     futuro[101]=0;
 for (int i=1; i<100;i++){
@@ -53,17 +54,39 @@ for (int i=1; i<100;i++){
 return futuro;
 }
 
+double* cambio(double* array, double* aray){
+    array=aray;
+    return array;
+}
+
 int main(){
     double Upasado[101];
     double Upresente[101];
-    //double Ufuturo[101];
+    double Ufuturo[101];
     cdin(Upasado, "posicioninicial.txt");
     uinit(Upresente, Upasado, "posicion1.txt");
-    //ofstream final("resultado.txt");
-
-    //for (int i; i<=300;i++){}
-
+    double* finall[300][101];
+    ofstream final("resultado.txt");
+    final << fixed << setprecision(6);
+    for (int j=0;j<101;j++){
+    final << Upasado[j];
+    }
+    final << endl;
+    for (int j=0;j<101;j++){
+    final << Upresente[j];
+    }
+    final <<endl;
+    for (int i=0; i<=298;i++){
+        paso(Upasado, Upresente, Ufuturo);
+        for (int m=0; m<=298;m++){
+              final  << Ufuturo[m];
+        }
+        final <<endl;
+        cambio(Upasado,Upresente);
+        cambio(Upresente,Ufuturo);
+    }
+    final.close();
     
-    cout << Upasado << Upresente;
+    cout << Upasado << Upresente<< Ufuturo;
     return 0;
 }
