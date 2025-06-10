83b1c7332592186cb3654c23c1f17ec9a71c1cba
Simonanchoa <siatehortuar@unal.edu.co>
2025-06-10 00:17:01 +0000

    avance final lunes

--- Semana9/cuerda.cpp
+++ Semana9/cuerda.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-double* condinicial(double* array, string nombre){
+double* cdin(double* array, string nombre){
     ofstream datos(nombre);
     datos << fixed << setprecision(3);
     datos << "posición"<< endl ;
@@ -29,9 +29,41 @@ double* condinicial(double* array, string nombre){
     return array;
 }
 
+double*  uinit(double* array, double* aray, string archivo){
+    ofstream unicial(archivo);
+    unicial << fixed << setprecision(6);
+    unicial << "posición"<< endl ;
+    array[0]=0;
+    array[101]=0;
+    unicial << array[0] << endl ;
+    for (int i=1; i<100;i++){
+        array[i]=aray[i]+0.125*(aray[i+1]-2*aray[i]+aray[i-1]);
+        unicial << array[i] << endl ;
+    }
+    unicial << array[101] << endl ;
+    unicial.close();
+    return array;
+}
+double* paso(double* pasado,double* presente, double* futuro,string datos){
+    futuro[0]=0;
+    futuro[101]=0;
+for (int i=1; i<100;i++){
+    futuro[i]=2*presente[i]-pasado[i]+0.25*(presente[i+1]-2*presente[i]+presente[i-1]);
+}
+return futuro;
+}
+
 int main(){
     double Upasado[101];
-    condinicial(Upasado, "posicioninicial.txt");
-    cout << Upasado;
+    double Upresente[101];
+    //double Ufuturo[101];
+    cdin(Upasado, "posicioninicial.txt");
+    uinit(Upresente, Upasado, "posicion1.txt");
+    //ofstream final("resultado.txt");
+
+    //for (int i; i<=300;i++){}
+
     
+    cout << Upasado << Upresente;
+    return 0;
 }
--- Semana9/graficas.py
+++ Semana9/graficas.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x=np.genfromtxt("posicioninicial.txt",skip_header=1) 
-
+x2=np.genfromtxt("posicion1.txt",skip_header=1) 
 pt=np.linspace(0,100,101)
 
 print("x:", x)
@@ -10,5 +10,5 @@ print("Shape of x:", x.shape)
 
 
 plt.figure()
-plt.plot(pt,x)
+plt.plot(pt,x,x2)
 plt.show()
