ad218dc6ac6ad8563445f81f116853352d8626c0
Simonanchoa <siatehortuar@unal.edu.co>
2025-06-19 04:31:26 +0000

    final

--- Semana9/placa.cpp
+++ Semana9/placa.cpp
@@ -39,13 +39,11 @@ void paso(double presente[][100], double futuro[][100], double dt, double dx, do
     }
     for(int i=1;i<99;i++){
         for (int j=1;j<99;j++){
-            futuro[i][j]=presente[i][j]+(v*dt/(dx*dx)*(presente[i+1][j]-2*presente[i][j]+presente[i-1][j]))+(v*dt/(dx*dx)*(presente[i][j+1]-2*presente[i][j]+presente[i][j-1]));
+            futuro[i][j]=presente[i][j]+(v*dt/(dx*dx)*(presente[i+1][j]-2*presente[i][j]+presente[i-1][j]+presente[i][j+1]-2*presente[i][j]+presente[i][j-1]));
         }
     }
  }
 
-#include <fstream>
-#include <iomanip>
 
 void guardar(double T[][100],string nombre_archivo) {
     ofstream archivo(nombre_archivo);
@@ -60,12 +58,26 @@ void guardar(double T[][100],string nombre_archivo) {
     archivo.close();
 }
 
+void cambio(double array[][100], double aray[][100]){
+    for (int i=1;i<100;i++){
+        for (int j=1;j<100;j++){
+            array[i][j]=aray[i][j];
 
+        }
+    }
+}
 int main (){
     double placa[100][100];
     double plate[100][100];
     cond_inicial(placa);
-    for (int i=)
-    paso(placa, plate, 1, 0.01,0.0001);
+    for (int i=0;i<=25000;i++){
+    paso(placa, plate, 0.1, 0.01,0.0001);
+    cambio(placa,plate);
+    if (i == 0) guardar(placa, "T_0.txt");
+    if (i == 1000) guardar(placa, "T_100.txt");
+    if (i == 10000) guardar(placa,"T_1000.txt");
+    if (i == 25000) guardar(placa,"T_2500.txt");
+    }
+    
     return 0;
 }
